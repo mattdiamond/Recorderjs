7b5ea9f2fdecb1356409f43b9e1ee82c87d8564c
Matt Diamond <mdiamond@onehealth.com>
2012-09-13 12:51:00 -0700

    implementing web worker per Issue #2

--- recorder.js
+++ recorder.js
@@ -1,18 +1,28 @@
 (function(window){
 
+  var WORKER_PATH = 'js/recorderjs/recorderWorker.js';
+
   var Recorder = function(source, bufferLen){
     bufferLen = bufferLen || 4096;
     this.context = source.context;
     this.node = this.context.createJavaScriptNode(bufferLen, 2, 2);
-    this.worker = new Worker('recorderWorker.js');
+    var worker = new Worker(WORKER_PATH);
     var recording = false;
+    var callback = null;
 
     this.node.onaudioprocess = function(e){
       if (!recording) return;
-      this.worker.postMessage({
+      worker.postMessage({
         command: 'record',
-        buffer: e.inputBuffer
+        buffer: [
+          e.inputBuffer.getChannelData(0),
+          e.inputBuffer.getChannelData(1)
+        ]
       });
+    }
+
+    this.setCallback = function(cb){
+      callback = cb;
     }
 
     this.record = function(){
@@ -24,23 +34,25 @@
     }
 
     this.clear = function(){
-      this.worker.postMessage({ command: 'clear' });
+      worker.postMessage({ command: 'clear' });
     }
 
-    this.exportWAV = function(){
-      this.worker.postMessage({ command: 'exportWAV' });
+    this.exportWAV = function(cb){
+      if (cb) callback = cb;
+      if (!callback) throw new Error('Callback not set');
+      worker.postMessage({ command: 'exportWAV' });
+    }
+
+    worker.onmessage = function(e){
+      var waveData = e.data;
+      var uri = "data:audio/wav;base64," + btoa(waveData);
+      callback(uri);
     }
 
     source.connect(this.node);
     this.node.connect(this.context.destination);    //this should not be necessary
-  }
+  };
 
   window.Recorder = Recorder;
-
-  window.onmessage = function(e){
-    var waveData = e.data;
-    var uri = "data:audio/wav;base64," + btoa(waveData);
-    window.open(uri);
-  }
 
 })(window);
