a63d96057e2f745078b5a50bb1335e661c4902cf
Daniel Demmel <daniel.demmel@gmail.com>
2012-11-17 19:38:15 +0000

    Added HTML example.

--- recorder.js
+++ recorder.js
@@ -1,6 +1,6 @@
 (function(window){
 
-  var WORKER_PATH = 'js/recorderjs/recorderWorker.js';
+  var WORKER_PATH = 'recorderWorker.js';
 
   var Recorder = function(source, cfg){
     var config = cfg || {};
