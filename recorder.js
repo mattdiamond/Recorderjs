9326beb2042eee2df903c0b96a5a771ea165969f
Chris <chris.rudmin@gmail.com>
2014-09-04 18:03:41 -0400

    Syntax Fixes
    Assignments should be terminated with semicolon

--- recorder.js
+++ recorder.js
@@ -28,7 +28,7 @@
           e.inputBuffer.getChannelData(1)
         ]
       });
-    }
+    };
 
     this.configure = function(cfg){
       for (var prop in cfg){
@@ -36,24 +36,24 @@
           config[prop] = cfg[prop];
         }
       }
-    }
+    };
 
     this.record = function(){
       recording = true;
-    }
+    };
 
     this.stop = function(){
       recording = false;
-    }
+    };
 
     this.clear = function(){
       worker.postMessage({ command: 'clear' });
-    }
+    };
 
     this.getBuffer = function(cb) {
       currCallback = cb || config.callback;
       worker.postMessage({ command: 'getBuffer' })
-    }
+    };
 
     this.exportWAV = function(cb, type){
       currCallback = cb || config.callback;
@@ -63,12 +63,12 @@
         command: 'exportWAV',
         type: type
       });
-    }
+    };
 
     worker.onmessage = function(e){
       var blob = e.data;
       currCallback(blob);
-    }
+    };
 
     source.connect(this.node);
     this.node.connect(this.context.destination);    //this should not be necessary
@@ -82,7 +82,7 @@
     var click = document.createEvent("Event");
     click.initEvent("click", true, true);
     link.dispatchEvent(click);
-  }
+  };
 
   window.Recorder = Recorder;
 
