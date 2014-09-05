3678597a0d943a2bb693c6555919516df23e0de7
Chris <chris.rudmin@gmail.com>
2014-09-04 23:21:37 -0400

    Moved to prototypical inheritance
    Avoids having the methods re-created for each instance of Recorder.
    Makes constructor code more concise.

--- recorder.js
+++ recorder.js
@@ -1,75 +1,25 @@
 var Recorder = function(source, config){
 
   var context = source.context,
-    isRecording = false,
-    node,
-    worker;
+    _this = this,
+    node;
 
   config = config || {};
   config.bufferLen = config.bufferLen || 4096;
   config.workerPath = config.workerPath || 'recorderWorker.js';
   config.mimeType = config.mimeType || 'audio/wav';
 
-  this.isRecording = function(){
-    return isRecording;
-  };
-
-  this.record = function(){
-    isRecording = true;
-  };
-
-  this.stop = function(){
-    isRecording = false;
-  };
-
-  this.clear = function(){
-    worker.postMessage({ command: 'clear' });
-  };
-
-  this.getBuffer = function(cb) {
-    var getBufferHandler = function(e){
-      e.stopPropagation();
-      worker.removeEventListener("message", getBufferHandler);
-      cb(e.data);
-    };
-    worker.addEventListener("message", getBufferHandler);
-    worker.postMessage({ command: 'getBuffer' })
-  };
-
-  this.exportWAV = function(cb, mimeType){
-    var exportWavHandler = function(e){
-      e.stopPropagation();
-      worker.removeEventListener("message", exportWavHandler);
-      cb(e.data);
-    };
-    worker.addEventListener("message", exportWavHandler);
-    worker.postMessage({
-      command: 'exportWAV',
-      type: mimeType || config.mimeType
-    });
-  };
-
-  worker = new Worker(config.workerPath);
-  worker.postMessage({
+  this.worker = new Worker(config.workerPath);
+  this.worker.postMessage({
     command: 'init',
     config: { sampleRate: context.sampleRate }
   });
 
   context.createScriptProcessor = context.createScriptProcessor || context.createJavaScriptNode;
   node = context.createScriptProcessor(config.bufferLen, 2, 2);
-  node.onaudioprocess = function(e){
-    if (isRecording) {
-      worker.postMessage({
-        command: 'record',
-        buffer: [
-          e.inputBuffer.getChannelData(0),
-          e.inputBuffer.getChannelData(1)
-        ]
-      });
-    }
-  };
+  node.onaudioprocess = function(e){ _this.recordSample(e.inputBuffer); };
   source.connect(node);
-  node.connect(context.destination);    //this should not be necessary
+  node.connect(context.destination);
 };
 
 Recorder.forceDownload = function(blob, filename){
@@ -80,4 +30,53 @@ Recorder.forceDownload = function(blob, filename){
   var click = document.createEvent("Event");
   click.initEvent("click", true, true);
   link.dispatchEvent(click);
+};
+
+Recorder.prototype.clear = function(){
+  this.worker.postMessage({ command: 'clear' });
+};
+
+Recorder.prototype.exportWAV = function(cb, mimeType){
+  var exportWavHandler = function(e){
+    e.stopPropagation();
+    worker.removeEventListener("message", exportWavHandler);
+    cb(e.data);
+  };
+  this.worker.addEventListener("message", exportWavHandler);
+  this.worker.postMessage({
+    command: 'exportWAV',
+    type: mimeType || config.mimeType
+  });
+};
+
+Recorder.prototype.getBuffer = function(cb) {
+  var getBufferHandler = function(e){
+    e.stopPropagation();
+    worker.removeEventListener("message", getBufferHandler);
+    cb(e.data);
+  };
+  this.worker.addEventListener("message", getBufferHandler);
+  this.worker.postMessage({ command: 'getBuffer' });
+};
+
+Recorder.prototype.isRecording = false;
+
+Recorder.prototype.recordSample = function(inputBuffer){
+  if (this.isRecording) {
+    this.worker.postMessage({
+      command: 'record',
+      buffer: [
+        inputBuffer.getChannelData(0),
+        inputBuffer.getChannelData(1)
+      ]
+    });
+  }
+};
+
+Recorder.prototype.start = function(){
+  this.isRecording = true;
+};
+
+Recorder.prototype.stop = function(){
+  this.isRecording = false;
 };
