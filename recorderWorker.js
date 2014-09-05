54f9f8f871dcf93b5d5db9cd02ce1e8617a76b8e
Chris <chris.rudmin@gmail.com>
2014-09-05 14:23:26 -0400

    Sorted functions alphabetically

--- recorderWorker.js
+++ recorderWorker.js
@@ -39,6 +39,44 @@ function clear(){
   }
 }
 
+function encodeWav(interleavedSamples){
+  var dataLength = interleavedSamples.length * 2,
+    buffer = new ArrayBuffer(44 + dataLength),
+    view = new DataView(buffer),
+    bitsPerSample = 16;
+
+  /* RIFF identifier */
+  writeString(view, 0, 'RIFF');
+  /* file length */
+  view.setUint32(4, 36 + dataLength, true);
+  /* RIFF type */
+  writeString(view, 8, 'WAVE');
+  /* format chunk identifier */
+  writeString(view, 12, 'fmt ');
+  /* format chunk length */
+  view.setUint32(16, 16, true);
+  /* sample format (raw) */
+  view.setUint16(20, 1, true);
+  /* channel count */
+  view.setUint16(22, numberOfChannels, true);
+  /* sample rate */
+  view.setUint32(24, sampleRate, true);
+  /* byte rate (sample rate * block align) */
+  view.setUint32(28, (sampleRate * bitsPerSample * numberOfChannels) / 8, true);
+  /* block align (channel count * bytes per sample) */
+  view.setUint16(32, (bitsPerSample * numberOfChannels) / 8, true);
+  /* bits per sample */
+  view.setUint16(34, bitsPerSample, true);
+  /* data chunk identifier */
+  writeString(view, 36, 'data');
+  /* data chunk length */
+  view.setUint32(40, dataLength, true);
+  /* PCM data */
+  floatTo16BitPCM(view, 44, interleavedSamples);
+
+  return view;
+}
+
 function getBuffer() {
   this.postMessage( mergeAllBuffers() );
 }
@@ -108,42 +146,4 @@ function writeString(view, offset, string){
   for (var i = 0; i < string.length; i++){
     view.setUint8(offset + i, string.charCodeAt(i));
   }
-}
-
-function encodeWav(interleavedSamples){
-  var dataLength = interleavedSamples.length * 2,
-    buffer = new ArrayBuffer(44 + dataLength),
-    view = new DataView(buffer),
-    bitsPerSample = 16;
-
-  /* RIFF identifier */
-  writeString(view, 0, 'RIFF');
-  /* file length */
-  view.setUint32(4, 36 + dataLength, true);
-  /* RIFF type */
-  writeString(view, 8, 'WAVE');
-  /* format chunk identifier */
-  writeString(view, 12, 'fmt ');
-  /* format chunk length */
-  view.setUint32(16, 16, true);
-  /* sample format (raw) */
-  view.setUint16(20, 1, true);
-  /* channel count */
-  view.setUint16(22, numberOfChannels, true);
-  /* sample rate */
-  view.setUint32(24, sampleRate, true);
-  /* byte rate (sample rate * block align) */
-  view.setUint32(28, (sampleRate * bitsPerSample * numberOfChannels) / 8, true);
-  /* block align (channel count * bytes per sample) */
-  view.setUint16(32, (bitsPerSample * numberOfChannels) / 8, true);
-  /* bits per sample */
-  view.setUint16(34, bitsPerSample, true);
-  /* data chunk identifier */
-  writeString(view, 36, 'data');
-  /* data chunk length */
-  view.setUint32(40, dataLength, true);
-  /* PCM data */
-  floatTo16BitPCM(view, 44, interleavedSamples);
-
-  return view;
 }
