265d3cc7b51237707d0f5dd10195d8c96ed48b34
MayP <mayppong@gmail.com>
2014-11-13 22:21:12 -0800

    Update documentation and comments
    Get it ready for a pull request.

--- recorderWorkerMP3.js
+++ recorderWorkerMP3.js
@@ -21,6 +21,11 @@ this.onmessage = function(e) {
   }
 };
 
+// The initilization default sample rate of the input audio at 44k
+// and scaled it down to 22k output. Not sure why this is necessary but is
+// needed for the sound to come out right.
+// The channels is set to mono and listenning to channel 1 which is the left channel.
+// There is some problem with stereo channel support.
 function init(config) {
   if (!config) {
     config = {};
@@ -43,6 +48,9 @@ function record(buffer) {
   recLength += mp3data.data.length;
 }
 
+// Similar to the original exportWAV, it grabs the mp3 data from Lame encoder
+// object and build an mp3 blob with it.
+// When done, it posts the audio blob message to the worker.
 function exportAudio() {
   var mp3data = Lame.encode_flush(mp3codec);
 
@@ -59,6 +67,8 @@ function exportAudio() {
   this.postMessage(audioBlob);
 }
 
+// Close the Lame encoder object stream, empty out the data in buffer and
+// reset the buffer length stored.
 function clear() {
   Lame.close(mp3codec);
   mp3codec = null;
