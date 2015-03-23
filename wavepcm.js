4096f567dd3337fbca4553cfda446a66da5397a7
Chris <chris.rudmin@gmail.com>
2015-03-23 00:24:49 -0400

    tweak remainder

--- wavepcm.js
+++ wavepcm.js
@@ -9,7 +9,7 @@
   this.recordedBuffers = [];
   this.bytesPerSample = this.bitDepth / 8;
   this.resampledBufferLength = Math.round( this.bufferLength * this.outputSampleRate / this.inputSampleRate );
-  this.resampleRatioRemainder  = ( this.bufferLength * this.outputSampleRate / this.inputSampleRate ) % 1;
+  this.resampleRatioRemainder  = 1 - ( this.bufferLength * this.outputSampleRate / this.inputSampleRate ) % 1;
   this.resampleRatio = (this.bufferLength-1) / (this.resampledBufferLength-1);
 
   this.lastSample = [];
