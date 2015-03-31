d4ba6a77db576177f9b329be4cbe8b659be7d63d
Chris <chris.rudmin@gmail.com>
2015-03-31 08:23:14 -0400

    restrict properties on error event

--- recorder.js
+++ recorder.js
@@ -161,7 +161,7 @@ Recorder.prototype.start = function(){
     Recorder.getUserMedia( 
       { audio : this.config.streamOptions },
       function( stream ){ that.onStreamInit( stream ); },
-      function( e ){ that.eventTarget.dispatchEvent( new ErrorEvent( "recordingError", { error: e, message: e.message, filename: e.fileName,lineno: e.lineNumber } ) ); }
+      function( e ){ that.eventTarget.dispatchEvent( new ErrorEvent( "recordingError", { error: e, message: e.message } ) ); }
     );
   };
 };
