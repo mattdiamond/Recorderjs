89f0e9fc9e788947b1f0e8068f4d33ffbafe05f7
perholje <per@holje.net>
2015-05-27 13:26:19 +0200

    fixed byte rate RIFF header value
    The byte rate calculation was hard-coded to 2 channels. Corrected this to include number of channels in the calculation.

--- recorderWorker.js
+++ recorderWorker.js
@@ -130,8 +130,8 @@ function encodeWAV(samples){
   view.setUint16(22, numChannels, true);
   /* sample rate */
   view.setUint32(24, sampleRate, true);
-  /* byte rate (sample rate * block align) */
-  view.setUint32(28, sampleRate * 4, true);
+  /* byte rate (sample rate * channels * bytes per sample) */
+  view.setUint32(28, sampleRate * numChannels * 2, true)
   /* block align (channel count * bytes per sample) */
   view.setUint16(32, numChannels * 2, true);
   /* bits per sample */
