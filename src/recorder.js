18586eb951143e20a4dba2a19a88686c9a261bd3
Saqib Shaikh <me@saqibshaikh.com>
2016-01-08 20:23:03 +0000

    In the wave header, bytes 29-32 were not being set correctly for mono audio

--- src/recorder.js
+++ src/recorder.js
@@ -173,7 +173,7 @@ export class Recorder {
                 /* sample rate */
                 view.setUint32(24, sampleRate, true);
                 /* byte rate (sample rate * block align) */
-                view.setUint32(28, sampleRate * 4, true);
+                view.setUint32(28, sampleRate * numChannels * 2, true);
                 /* block align (channel count * bytes per sample) */
                 view.setUint16(32, numChannels * 2, true);
                 /* bits per sample */
