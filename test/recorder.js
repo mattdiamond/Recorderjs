a262d43840b58cb0ca076e26d18d76479e7c13fa
Chris Rudmin <chris.rudmin@gmail.com>
2017-04-04 11:31:08 -0400

    Fix tabs and indentation

--- test/recorder.js
+++ test/recorder.js
@@ -12,13 +12,14 @@ describe('Recorder unsupported', function(){
 
 	var Recorder = require('../dist/recorder.min');
 
-    it('should not support Recording', function () {
-    	expect(Recorder.isRecordingSupported()).to.not.be.ok;
-    });
+  it('should not support Recording', function () {
+  	expect(Recorder.isRecordingSupported()).to.not.be.ok;
+  });
 
 	it('should throw an error', function () {
-    	expect(Recorder).to.throw("Recording is not supported in this browser");
-    });
+  	expect(Recorder).to.throw("Recording is not supported in this browser");
+  });
+
 });
 
 describe('Recorder', function(){
@@ -62,64 +63,64 @@ describe('Recorder', function(){
 		Recorder = requireUncached('../dist/recorder.min');
 	});
 
-    afterEach(function () {
-        sandbox.restore();
-    });
+  afterEach(function () {
+    sandbox.restore();
+  });
 
 	it('should support Recording', function () {
-    	expect(Recorder.isRecordingSupported()).to.be.ok;
-    });
+  	expect(Recorder.isRecordingSupported()).to.be.ok;
+  });
 
 	it('should create an instance with default config', function () {
 		var rec = new Recorder();
-    	expect(global.AudioContext).to.have.been.calledWithNew;
-    	expect(rec.state).to.equal('inactive');
-    	expect(rec.config).to.have.property('bufferLength', 4096);
-    	expect(rec.config).to.have.property('monitorGain', 0);
-    	expect(rec.config).to.have.property('numberOfChannels', 1);
-    	expect(rec.config).to.have.property('encoderSampleRate', 48000);
-    	expect(rec.config).to.have.property('encoderPath', 'encoderWorker.min.js');
-    	expect(rec.config).to.have.property('streamPages', false);
-    	expect(rec.config).to.have.property('leaveStreamOpen', false);
-    	expect(rec.config).to.have.property('maxBuffersPerPage', 40);
-    	expect(rec.config).to.have.property('encoderApplication', 2049);
-    	expect(rec.config).to.have.property('encoderFrameSize', 20);
-    	expect(rec.config).to.have.property('resampleQuality', 3);
-    });
+  	expect(global.AudioContext).to.have.been.calledWithNew;
+  	expect(rec.state).to.equal('inactive');
+  	expect(rec.config).to.have.property('bufferLength', 4096);
+  	expect(rec.config).to.have.property('monitorGain', 0);
+  	expect(rec.config).to.have.property('numberOfChannels', 1);
+  	expect(rec.config).to.have.property('encoderSampleRate', 48000);
+  	expect(rec.config).to.have.property('encoderPath', 'encoderWorker.min.js');
+  	expect(rec.config).to.have.property('streamPages', false);
+  	expect(rec.config).to.have.property('leaveStreamOpen', false);
+  	expect(rec.config).to.have.property('maxBuffersPerPage', 40);
+  	expect(rec.config).to.have.property('encoderApplication', 2049);
+  	expect(rec.config).to.have.property('encoderFrameSize', 20);
+  	expect(rec.config).to.have.property('resampleQuality', 3);
+  });
 
 	it('should create an instance with custom config', function () {
 		var rec = new Recorder({
 			bufferLength: 2048,
-	        monitorGain: 100,
-	        numberOfChannels: 2,
-	        bitRate: 16000,
-	        encoderSampleRate: 16000,
-	        encoderPath: "../dist/encoderWorker.min.js",
-	        streamPages: true,
-	        leaveStreamOpen: false,
-	        maxBuffersPerPage: 1000,
-	        encoderApplication: 2048,
-	        encoderFrameSize: 40,
-	        resampleQuality: 10
+      monitorGain: 100,
+      numberOfChannels: 2,
+      bitRate: 16000,
+      encoderSampleRate: 16000,
+      encoderPath: "../dist/encoderWorker.min.js",
+      streamPages: true,
+      leaveStreamOpen: false,
+      maxBuffersPerPage: 1000,
+      encoderApplication: 2048,
+      encoderFrameSize: 40,
+      resampleQuality: 10
 		});
 
-    	expect(global.AudioContext).to.have.been.calledWithNew;
-    	expect(rec.state).to.equal('inactive');
-    	expect(rec.config).to.have.property('bufferLength', 2048);
-    	expect(rec.config).to.have.property('monitorGain', 100);
-    	expect(rec.config).to.have.property('numberOfChannels', 2);
-    	expect(rec.config).to.have.property('bitRate', 16000);
-    	expect(rec.config).to.have.property('encoderSampleRate', 16000);
-    	expect(rec.config).to.have.property('encoderPath', '../dist/encoderWorker.min.js');
-    	expect(rec.config).to.have.property('streamPages', true);
-    	expect(rec.config).to.have.property('leaveStreamOpen', false);
-    	expect(rec.config).to.have.property('maxBuffersPerPage', 1000);
-    	expect(rec.config).to.have.property('encoderApplication', 2048);
-    	expect(rec.config).to.have.property('encoderFrameSize', 40);
-    	expect(rec.config).to.have.property('resampleQuality', 10);
-    });
+  	expect(global.AudioContext).to.have.been.calledWithNew;
+  	expect(rec.state).to.equal('inactive');
+  	expect(rec.config).to.have.property('bufferLength', 2048);
+  	expect(rec.config).to.have.property('monitorGain', 100);
+  	expect(rec.config).to.have.property('numberOfChannels', 2);
+  	expect(rec.config).to.have.property('bitRate', 16000);
+  	expect(rec.config).to.have.property('encoderSampleRate', 16000);
+  	expect(rec.config).to.have.property('encoderPath', '../dist/encoderWorker.min.js');
+  	expect(rec.config).to.have.property('streamPages', true);
+  	expect(rec.config).to.have.property('leaveStreamOpen', false);
+  	expect(rec.config).to.have.property('maxBuffersPerPage', 1000);
+  	expect(rec.config).to.have.property('encoderApplication', 2048);
+  	expect(rec.config).to.have.property('encoderFrameSize', 40);
+  	expect(rec.config).to.have.property('resampleQuality', 10);
+  });
 
-    it('should initialize a new audio stream', function () {
+  it('should initialize a new audio stream', function () {
 		var rec = new Recorder();
 		rec.initStream();
 		expect(rec.stream).to.be.defined;
@@ -128,9 +129,9 @@ describe('Recorder', function(){
 		expect(rec.eventTarget.dispatchEvent).to.have.been.calledOnce;
 		expect(global.Event).to.have.been.calledOnce;
 		expect(global.Event).to.have.been.calledWith("streamReady");
-    });
+  });
 
-    it('should use the existing audio stream if already initialized', function () {
+  it('should use the existing audio stream if already initialized', function () {
 		var rec = new Recorder();
 		rec.initStream();
 		rec.initStream();
@@ -140,48 +141,48 @@ describe('Recorder', function(){
 		expect(rec.eventTarget.dispatchEvent).to.have.been.calledTwice;
 		expect(global.Event).to.have.been.calledTwice;
 		expect(global.Event).to.have.been.calledWith("streamReady");
-    });
+  });
 
-    it('should clear the audio stream', function () {
+  it('should clear the audio stream', function () {
 		var rec = new Recorder();
 		rec.initStream();
 		rec.clearStream();
 		expect(rec.stream).to.be.undefined;
-    });
+  });
 
-    it('should clear the audio stream when stream contains tracks', function () {
-    	var stopTrack1 = sandbox.stub();
-    	var stopTrack2 = sandbox.stub();
+  it('should clear the audio stream when stream contains tracks', function () {
+  	var stopTrack1 = sandbox.stub();
+  	var stopTrack2 = sandbox.stub();
 		global.navigator.getUserMedia = sandbox.stub().yields({
-			getTracks: sandbox.stub().returns([
-				{ stop: stopTrack1 },
-				{ stop: stopTrack2 }
-			])
-		});
+		getTracks: sandbox.stub().returns([
+			{ stop: stopTrack1 },
+			{ stop: stopTrack2 }
+		])
+	});
 
-		var rec = new Recorder();
+	var rec = new Recorder();
 		rec.initStream();
 		rec.clearStream();
 		expect(stopTrack1).to.have.been.calledOnce;
 		expect(stopTrack2).to.have.been.calledOnce;
 		expect(rec.stream).to.be.undefined;
-    });
+  });
 
-    it('should add an event listener', function () {
+  it('should add an event listener', function () {
 		var rec = new Recorder();
 		rec.addEventListener( "a", "b");
 		expect(rec.eventTarget.addEventListener).to.have.been.calledOnce;
 		expect(rec.eventTarget.addEventListener).to.have.been.calledWith("a", "b", undefined);
-    });
+  });
 
-    it('should remove an event listener', function () {
+  it('should remove an event listener', function () {
 		var rec = new Recorder();
 		rec.removeEventListener( "a", "b");
 		expect(rec.eventTarget.removeEventListener).to.have.been.calledOnce;
 		expect(rec.eventTarget.removeEventListener).to.have.been.calledWith("a", "b", undefined);
-    });
+  });
 
-    it('should start recording', function () {
+  it('should start recording', function () {
 		var rec = new Recorder();
 		rec.initStream();
 		rec.start();
@@ -189,9 +190,9 @@ describe('Recorder', function(){
 		expect(rec.encoder.addEventListener).to.have.been.calledOnce;
 		expect(rec.encoder.addEventListener).to.have.been.calledWith('message');
 		expect(rec.state).to.equal('recording');
-        expect(rec.scriptProcessorNode.connect).to.have.been.calledWith( rec.audioContext.destination );
-        expect(global.Event).to.have.been.calledWith('start');
-        expect(rec.encoder.postMessage).to.have.been.calledWith( rec.config );
-    });
+    expect(rec.scriptProcessorNode.connect).to.have.been.calledWith( rec.audioContext.destination );
+    expect(global.Event).to.have.been.calledWith('start');
+    expect(rec.encoder.postMessage).to.have.been.calledWith( rec.config );
+  });
 
 });
