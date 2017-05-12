38be1e843f0e4827bb1de331fe2d0dbca80971ec
Chris Rudmin <chris.rudmin@gmail.com>
2017-05-12 15:21:17 -0400

    Make encoderWorker testable

--- test/encoderWorker.js
+++ test/encoderWorker.js
@@ -2,6 +2,7 @@ var chai = require('chai');
 var sinon = require('sinon');
 var sinonChai = require("sinon-chai");
 var requireUncached = require('require-uncached');
+var Promise = require('promise');
 
 chai.use(sinonChai);
 var should = chai.should();
@@ -9,5 +10,21 @@ var expect = chai.expect;
 
 
 describe('encoderWorker', function() {
+
+  var encoderWorker = require('../dist/encoderWorker.min');
+
+  beforeEach(function(){
+    global = {}
+    global.postMessage = sandbox.stub();
+    global.close = sandbox.stub();
+  });
+
+  afterEach(function () {
+    sandbox.restore();
+  });
+
+  it('should initialize', function () {
+    expect(encoderWorker).to.be.defined;
+  });
 
 });
