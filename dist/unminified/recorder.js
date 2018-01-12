021aff15495009be27f93a9f944496dd79222649
Igor Zhukov <igor.beatle@gmail.com>
2018-01-12 16:05:35 +0400

    Added compiled unminified sources to dist
    Closes #126

---
+++ dist/unminified/recorder.js
@@ -0,0 +1,452 @@
+(function webpackUniversalModuleDefinition(root, factory) {
+	if(typeof exports === 'object' && typeof module === 'object')
+		module.exports = factory();
+	else if(typeof define === 'function' && define.amd)
+		define([], factory);
+	else if(typeof exports === 'object')
+		exports["Recorder"] = factory();
+	else
+		root["Recorder"] = factory();
+})(typeof self !== 'undefined' ? self : this, function() {
+return /******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, {
+/******/ 				configurable: false,
+/******/ 				enumerable: true,
+/******/ 				get: getter
+/******/ 			});
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = 0);
+/******/ })
+/************************************************************************/
+/******/ ([
+/* 0 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+/* WEBPACK VAR INJECTION */(function(global) {
+
+var getUserMedia = __webpack_require__(2);
+var AudioContext = global.AudioContext || global.webkitAudioContext;
+
+var Recorder = function( config ){
+
+  var that = this;
+
+  if ( !Recorder.isRecordingSupported() ) {
+    throw new Error("Recording is not supported in this browser");
+  }
+
+  this.state = "inactive";
+  this.eventTarget = global.document.createDocumentFragment();
+  this.monitorNode = this.audioContext.createGain();
+  this.config = Object.assign({
+    bufferLength: 4096,
+    encoderApplication: 2049,
+    encoderFrameSize: 20,
+    encoderPath: 'encoderWorker.min.js',
+    encoderSampleRate: 48000,
+    leaveStreamOpen: false,
+    maxBuffersPerPage: 40,
+    monitorGain: 0,
+    numberOfChannels: 1,
+    originalSampleRate: this.audioContext.sampleRate,
+    resampleQuality: 3,
+    mediaTrackConstraints: true,
+    streamPages: false,
+    wavBitDepth: 16,
+    wavSampleRate: this.audioContext.sampleRate
+  }, config );
+
+  this.initWorker();
+  this.setMonitorGain( this.config.monitorGain );
+  this.scriptProcessorNode = this.audioContext.createScriptProcessor( this.config.bufferLength, this.config.numberOfChannels, this.config.numberOfChannels );
+  this.scriptProcessorNode.onaudioprocess = function( e ){
+    that.encodeBuffers( e.inputBuffer );
+  };
+};
+
+Recorder.isRecordingSupported = function(){
+  return AudioContext && getUserMedia.isSupported;
+};
+
+Recorder.prototype.addEventListener = function( type, listener, useCapture ){
+  this.eventTarget.addEventListener( type, listener, useCapture );
+};
+
+Recorder.prototype.audioContext = Recorder.isRecordingSupported() && new AudioContext();
+
+Recorder.prototype.clearStream = function() {
+  if ( this.stream ) {
+
+    if ( this.stream.getTracks ) {
+      this.stream.getTracks().forEach(function ( track ) {
+        track.stop();
+      });
+    }
+
+    else {
+      this.stream.stop();
+    }
+
+    delete this.stream;
+  }
+};
+
+Recorder.prototype.encodeBuffers = function( inputBuffer ){
+  if ( this.state === "recording" ) {
+    var buffers = [];
+    for ( var i = 0; i < inputBuffer.numberOfChannels; i++ ) {
+      buffers[i] = inputBuffer.getChannelData(i);
+    }
+
+    this.encoder.postMessage({
+      command: "encode",
+      buffers: buffers
+    });
+  }
+};
+
+Recorder.prototype.initStream = function(){
+  var that = this;
+
+  var onStreamInit = function( stream ){
+    that.stream = stream;
+    that.sourceNode = that.audioContext.createMediaStreamSource( stream );
+    that.sourceNode.connect( that.scriptProcessorNode );
+    that.sourceNode.connect( that.monitorNode );
+    that.eventTarget.dispatchEvent( new global.Event( "streamReady" ) );
+    return stream;
+  };
+
+  var onStreamError = function( e ){
+    that.eventTarget.dispatchEvent( new global.ErrorEvent( "streamError", { error: e } ) );
+    throw e;
+  };
+
+  var constraints = { audio : this.config.mediaTrackConstraints };
+
+  if ( this.stream ) {
+    this.eventTarget.dispatchEvent( new global.Event( "streamReady" ) );
+    return global.Promise.resolve( this.stream );
+  }
+
+  return getUserMedia(constraints).then( onStreamInit, onStreamError );
+};
+
+Recorder.prototype.initWorker = function(){
+  var that = this;
+  this.encoder = new global.Worker( this.config.encoderPath );
+
+  if (this.config.streamPages){
+    this.encoder.addEventListener( "message", function( e ) {
+      that.streamPage( e.data );
+    });
+  }
+
+  else {
+    this.recordedPages = [];
+    this.totalLength = 0;
+    this.encoder.addEventListener( "message", function( e ) {
+      that.storePage( e.data );
+    });
+  }
+};
+
+Recorder.prototype.pause = function(){
+  if ( this.state === "recording" ){
+    this.state = "paused";
+    this.eventTarget.dispatchEvent( new global.Event( 'pause' ) );
+  }
+};
+
+Recorder.prototype.removeEventListener = function( type, listener, useCapture ){
+  this.eventTarget.removeEventListener( type, listener, useCapture );
+};
+
+Recorder.prototype.resume = function() {
+  if ( this.state === "paused" ) {
+    this.state = "recording";
+    this.eventTarget.dispatchEvent( new global.Event( 'resume' ) );
+  }
+};
+
+Recorder.prototype.setMonitorGain = function( gain ){
+  this.monitorNode.gain.setTargetAtTime(gain, this.audioContext.currentTime, 0.01);
+};
+
+Recorder.prototype.start = function(){
+  if ( this.state === "inactive" && this.stream ) {
+
+    this.encoder.postMessage(Object.assign({
+      command: 'init'
+    }, this.config));
+
+    // First buffer can contain old data. Don't encode it.
+    this.encodeBuffers = function(){
+      delete this.encodeBuffers;
+    };
+
+    this.state = "recording";
+    this.monitorNode.connect( this.audioContext.destination );
+    this.scriptProcessorNode.connect( this.audioContext.destination );
+    this.eventTarget.dispatchEvent( new global.Event( 'start' ) );
+  }
+};
+
+Recorder.prototype.stop = function(){
+  if ( this.state !== "inactive" ) {
+    this.state = "inactive";
+    this.monitorNode.disconnect();
+    this.scriptProcessorNode.disconnect();
+
+    if ( !this.config.leaveStreamOpen ) {
+      this.clearStream();
+    }
+
+    this.encoder.postMessage({ command: "done" });
+  }
+};
+
+Recorder.prototype.storePage = function( page ) {
+  if ( page === null ) {
+    var outputData = new Uint8Array( this.totalLength );
+    var outputIndex = 0;
+
+    for ( var i = 0; i < this.recordedPages.length; i++ ) {
+      outputData.set( this.recordedPages[i], outputIndex );
+      outputIndex += this.recordedPages[i].length;
+    }
+
+    this.eventTarget.dispatchEvent( new global.CustomEvent( 'dataAvailable', {
+      detail: outputData
+    }));
+
+    this.initWorker();
+    this.eventTarget.dispatchEvent( new global.Event( 'stop' ) );
+  }
+
+  else {
+    this.recordedPages.push( page );
+    this.totalLength += page.length;
+  }
+};
+
+Recorder.prototype.streamPage = function( page ) {
+  if ( page === null ) {
+    this.initWorker();
+    this.eventTarget.dispatchEvent( new global.Event( 'stop' ) );
+  }
+
+  else {
+    this.eventTarget.dispatchEvent( new global.CustomEvent( 'dataAvailable', {
+      detail: page
+    }));
+  }
+};
+
+
+module.exports = Recorder;
+
+/* WEBPACK VAR INJECTION */}.call(exports, __webpack_require__(1)))
+
+/***/ }),
+/* 1 */
+/***/ (function(module, exports) {
+
+var g;
+
+// This works in non-strict mode
+g = (function() {
+	return this;
+})();
+
+try {
+	// This works if eval is allowed (see CSP)
+	g = g || Function("return this")() || (1,eval)("this");
+} catch(e) {
+	// This works if the window reference is available
+	if(typeof window === "object")
+		g = window;
+}
+
+// g can still be undefined, but nothing to do about it...
+// We return undefined, instead of nothing here, so it's
+// easier to handle this case. if(!global) { ...}
+
+module.exports = g;
+
+
+/***/ }),
+/* 2 */
+/***/ (function(module, exports, __webpack_require__) {
+
+var __WEBPACK_AMD_DEFINE_ARRAY__, __WEBPACK_AMD_DEFINE_RESULT__;// loosely based on example code at https://developer.mozilla.org/en-US/docs/Web/API/MediaDevices/getUserMedia
+(function (root) {
+  'use strict';
+
+  /**
+   * Error thrown when any required feature is missing (Promises, navigator, getUserMedia)
+   * @constructor
+   */
+  function NotSupportedError() {
+    this.name = 'NotSupportedError';
+    this.message = 'getUserMedia is not implemented in this browser';
+  }
+  NotSupportedError.prototype = Error.prototype;
+
+  /**
+   * Fake Promise instance that behaves like a Promise except that it always rejects with a NotSupportedError.
+   * Used for situations where there is no global Promise constructor.
+   *
+   * The message will report that the getUserMedia API is not available.
+   * This is technically true because every browser that supports getUserMedia also supports promises.
+   **
+   * @see http://caniuse.com/#feat=stream
+   * @see http://caniuse.com/#feat=promises
+   * @constructor
+   */
+  function FakePromise() {
+    // make it chainable like a real promise
+    this.then = function() {
+      return this;
+    };
+
+    // but always reject with an error
+    var err = new NotSupportedError();
+    this.catch = function(cb) {
+      setTimeout(function () {
+        cb(err);
+      });
+    }
+  }
+
+  var isPromiseSupported = typeof Promise !== 'undefined';
+
+  // checks for root.navigator to enable server-side rendering of things that depend on this
+  // (will need to be updated on client, but at least doesn't throw this way)
+  var navigatorExists = typeof navigator !== "undefined";
+  // gump = mondern promise-based interface
+  // gum = old callback-based interface
+  var gump = navigatorExists && navigator.mediaDevices && navigator.mediaDevices.getUserMedia;
+  var gum = navigatorExists && (navigator.getUserMedia || navigator.webkitGetUserMedia ||  navigator.mozGetUserMedia || navigator.msGetUserMedia);
+
+  /**
+   * Wrapper for navigator.mediaDevices.getUserMedia.
+   * Always returns a Promise or Promise-like object, even in environments without a global Promise constructor
+   *
+   * @stream https://developer.mozilla.org/en-US/docs/Web/API/MediaDevices/getUserMedia
+   *
+   * @param {Object} constraints - must include one or both of audio/video along with optional details for video
+   * @param {Boolean} [constraints.audio] - include audio data in the stream
+   * @param {Boolean|Object} [constraints.video] - include video data in the stream. May be a boolean or an object with additional constraints, see
+   * @returns {Promise<MediaStream>} a promise that resolves to a MediaStream object
+     */
+  function getUserMedia(constraints) {
+    // ensure that Promises are supported and we have a navigator object
+    if (!isPromiseSupported) {
+      return new FakePromise();
+    }
+
+    // Try the more modern, promise-based MediaDevices API first
+    //https://developer.mozilla.org/en-US/docs/Web/API/MediaDevices/getUserMedia
+    if(gump) {
+      return navigator.mediaDevices.getUserMedia(constraints);
+    }
+
+    // fall back to the older method second, wrap it in a promise.
+    return new Promise(function(resolve, reject) {
+      // if navigator doesn't exist, then we can't use the getUserMedia API. (And probably aren't even in a browser.)
+      // assuming it does, try getUserMedia and then all of the prefixed versions
+
+      if (!gum) {
+        return reject(new NotSupportedError())
+      }
+      gum.call(navigator, constraints, resolve, reject);
+    });
+  }
+
+  getUserMedia.NotSupportedError = NotSupportedError;
+
+  // eslint-disable-next-line no-implicit-coercion
+  getUserMedia.isSupported = !!(isPromiseSupported && (gump || gum));
+
+  // UMD, loosely based on https://github.com/umdjs/umd/blob/master/templates/returnExportsGlobal.js
+  if (true) {
+    // AMD. Register as an anonymous module.
+    !(__WEBPACK_AMD_DEFINE_ARRAY__ = [], __WEBPACK_AMD_DEFINE_RESULT__ = (function () {
+      return getUserMedia;
+    }).apply(exports, __WEBPACK_AMD_DEFINE_ARRAY__),
+				__WEBPACK_AMD_DEFINE_RESULT__ !== undefined && (module.exports = __WEBPACK_AMD_DEFINE_RESULT__));
+  } else if (typeof module === 'object' && module.exports) {
+    // Node. Does not work with strict CommonJS, but
+    // only CommonJS-like enviroments that support module.exports,
+    // like Node.
+    module.exports = getUserMedia;
+  } else {
+    // Browser globals
+    // polyfill the MediaDevices API if it does not exist.
+    root.navigator = root.navigator || {};
+    root.nagivator.mediaDevices = root.navigator.mediaDevices || {};
+    root.nagivator.mediaDevices.getUserMedia = root.nagivator.mediaDevices.getUserMedia || getUserMedia;
+  }
+}(this));
+
+
+/***/ })
+/******/ ]);
+});
---
+++ dist/unminified/waveWorker.js
@@ -0,0 +1,250 @@
+(function webpackUniversalModuleDefinition(root, factory) {
+	if(typeof exports === 'object' && typeof module === 'object')
+		module.exports = factory();
+	else if(typeof define === 'function' && define.amd)
+		define([], factory);
+	else if(typeof exports === 'object')
+		exports["WaveWorker"] = factory();
+	else
+		root["WaveWorker"] = factory();
+})(typeof self !== 'undefined' ? self : this, function() {
+return /******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, {
+/******/ 				configurable: false,
+/******/ 				enumerable: true,
+/******/ 				get: getter
+/******/ 			});
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = 0);
+/******/ })
+/************************************************************************/
+/******/ ([
+/* 0 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+/* WEBPACK VAR INJECTION */(function(global) {
+  
+var recorder;
+
+global['onmessage'] = function( e ){
+  switch( e['data']['command'] ){
+
+    case 'encode':
+      if (recorder) {
+        recorder.record( e['data']['buffers'] );
+      }
+      break;
+
+    case 'done':
+      if (recorder) {
+        recorder.requestData();
+      }
+      break;
+
+    case 'init':
+      recorder = new WavePCM( e['data'] );
+      break;
+
+    default:
+      // Ignore any unknown commands and continue recieving commands
+  }
+};
+
+var WavePCM = function( config ){
+
+  var config = Object.assign({
+    wavBitDepth: 16
+  }, config);
+
+  if ( !config['wavSampleRate'] ) {
+    throw new Error("wavSampleRate value is required to record. NOTE: Audio is not resampled!");
+  }
+
+  if ( [8, 16, 24, 32].indexOf( config['wavBitDepth'] ) === -1 ) {
+    throw new Error("Only 8, 16, 24 and 32 bits per sample are supported");
+  }
+
+  this.bitDepth = config['wavBitDepth'];
+  this.sampleRate = config['wavSampleRate'];
+  this.recordedBuffers = [];
+  this.bytesPerSample = this.bitDepth / 8;
+};
+
+WavePCM.prototype.record = function( buffers ){
+  this.numberOfChannels = this.numberOfChannels || buffers.length;
+  var bufferLength = buffers[0].length;
+  var reducedData = new Uint8Array( bufferLength * this.numberOfChannels * this.bytesPerSample );
+
+  // Interleave
+  for ( var i = 0; i < bufferLength; i++ ) {
+    for ( var channel = 0; channel < this.numberOfChannels; channel++ ) {
+
+      var outputIndex = ( i * this.numberOfChannels + channel ) * this.bytesPerSample;
+      var sample = buffers[ channel ][ i ];
+
+      // Check for clipping
+      if ( sample > 1 ) {
+        sample = 1;
+      }
+
+      else if ( sample < -1 ) {
+        sample = -1;
+      }
+
+      // bit reduce and convert to uInt
+      switch ( this.bytesPerSample ) {
+        case 4:
+          sample = sample * 2147483648;
+          reducedData[ outputIndex ] = sample;
+          reducedData[ outputIndex + 1 ] = sample >> 8;
+          reducedData[ outputIndex + 2 ] = sample >> 16;
+          reducedData[ outputIndex + 3 ] = sample >> 24;
+          break;
+
+        case 3:
+          sample = sample * 8388608;
+          reducedData[ outputIndex ] = sample;
+          reducedData[ outputIndex + 1 ] = sample >> 8;
+          reducedData[ outputIndex + 2 ] = sample >> 16;
+          break;
+
+        case 2:
+          sample = sample * 32768;
+          reducedData[ outputIndex ] = sample;
+          reducedData[ outputIndex + 1 ] = sample >> 8;
+          break;
+
+        case 1:
+          reducedData[ outputIndex ] = ( sample + 1 ) * 128; // 8 bit is signed integer.
+          break;
+
+        default:
+          throw new Error("Only 8, 16, 24 and 32 bits per sample are supported");
+      }
+    }
+  }
+
+  this.recordedBuffers.push( reducedData );
+};
+
+WavePCM.prototype.requestData = function(){
+  var bufferLength = this.recordedBuffers[0].length;
+  var dataLength = this.recordedBuffers.length * bufferLength;
+  var headerLength = 44;
+  var wav = new Uint8Array( headerLength + dataLength );
+  var view = new DataView( wav.buffer );
+
+  view.setUint32( 0, 1380533830, false ); // RIFF identifier 'RIFF'
+  view.setUint32( 4, 36 + dataLength, true ); // file length minus RIFF identifier length and file description length
+  view.setUint32( 8, 1463899717, false ); // RIFF type 'WAVE'
+  view.setUint32( 12, 1718449184, false ); // format chunk identifier 'fmt '
+  view.setUint32( 16, 16, true ); // format chunk length
+  view.setUint16( 20, 1, true ); // sample format (raw)
+  view.setUint16( 22, this.numberOfChannels, true ); // channel count
+  view.setUint32( 24, this.sampleRate, true ); // sample rate
+  view.setUint32( 28, this.sampleRate * this.bytesPerSample * this.numberOfChannels, true ); // byte rate (sample rate * block align)
+  view.setUint16( 32, this.bytesPerSample * this.numberOfChannels, true ); // block align (channel count * bytes per sample)
+  view.setUint16( 34, this.bitDepth, true ); // bits per sample
+  view.setUint32( 36, 1684108385, false); // data chunk identifier 'data'
+  view.setUint32( 40, dataLength, true ); // data chunk length
+
+  for (var i = 0; i < this.recordedBuffers.length; i++ ) {
+    wav.set( this.recordedBuffers[i], i * bufferLength + headerLength );
+  }
+
+  global['postMessage']( wav, [wav.buffer] );
+  global['postMessage'](null);
+  global['close']();
+};
+
+
+module.exports = WavePCM
+
+/* WEBPACK VAR INJECTION */}.call(exports, __webpack_require__(1)))
+
+/***/ }),
+/* 1 */
+/***/ (function(module, exports) {
+
+var g;
+
+// This works in non-strict mode
+g = (function() {
+	return this;
+})();
+
+try {
+	// This works if eval is allowed (see CSP)
+	g = g || Function("return this")() || (1,eval)("this");
+} catch(e) {
+	// This works if the window reference is available
+	if(typeof window === "object")
+		g = window;
+}
+
+// g can still be undefined, but nothing to do about it...
+// We return undefined, instead of nothing here, so it's
+// easier to handle this case. if(!global) { ...}
+
+module.exports = g;
+
+
+/***/ })
+/******/ ]);
+});
